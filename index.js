7ce86d04b7d13ff6715ca747978a1198c865754e
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2023-12-22 19:08:25 +0600

    added file

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // middleWare
 app.use(express.json());
 app.use(cors());
+
+
 
 const uri = `mongodb+srv://Job-Task-Scic:7gxWwBVoRVs650ny@cluster0.2amfc4s.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -29,11 +31,39 @@ async function run() {
         const result = await TaskCollection.find().toArray()
         res.send(result)
     })
+    app.get('/CardEdit/:id',async(req,res)=>{
+      const id = req.params.id 
+      console.log(id);
+      const query = {_id:new ObjectId(id)}
+      const result = await TaskCollection.findOne(query)
+      res.send(result)
+    })
     app.post("/createTask", async (req, res) => {
         const task = req.body 
         const result = await TaskCollection.insertOne(task)
         res.send(result)
     });
+    app.patch('/edit/:id',async(req,res)=>{
+      const data = req.body 
+      const id = req.params.id 
+      const query = {_id:new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          title:data.title,
+          priority:data.priority,
+          descriptions: data.descriptions,
+          dateInput:data.dateInput
+        },
+      };
+      const result = await TaskCollection.updateOne(query,updateDoc)
+      res.send(result)
+    })
+    app.delete('/delete/:id',async(req,res)=>{
+      const id = req.params.id
+      const query = {_id:new ObjectId(id)}
+      const result = await TaskCollection.deleteOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
